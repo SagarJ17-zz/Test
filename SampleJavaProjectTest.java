051c39ed33a10e0925be55e336ee14b643c77940
SagarJ17 <71249634+SagarJ17@users.noreply.github.com>
2021-07-23 14:30:57 +0530

    updated

--- SampleJavaProjectTest.java
+++ SampleJavaProjectTest.java
@@ -5,36 +5,17 @@ import static org.junit.Assert.assertNull;
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Sample JUnit tests.
- */
 public class SampleJavaProjectTest {
-
-    /**
-     * Holds an instance of the class we are testing.
-     */
     private SampleJavaProject sjp;
-
-    /**
-     * JUnit set up method.
-     */
     @Before
     public final void setUp() {
         sjp = new SampleJavaProject();
     }
-
-    /**
-     * Tests the generated setter and getter methods.
-     */
     @Test
     public final void testGetSet() {
         sjp.setName("foo");
         assertEquals("foo", sjp.getName());
     }
-
-    /**
-     * Tests that the null check in the setter.
-     */
     @Test(expected=NullPointerException.class)
     public final void nullTest() {
         sjp.setName(null);
